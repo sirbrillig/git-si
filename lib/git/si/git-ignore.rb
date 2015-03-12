f57023b9c393d11d10f5ac84a243a877154b1bbf
Payton Swick <payton@foolord.com>
2015-03-12 12:30:16 -0400

    Add GitIgnore.get_missing_lines_from

--- lib/git/si/git-ignore.rb
+++ lib/git/si/git-ignore.rb
@@ -7,6 +7,14 @@ module Git
         @@gitignore_patterns
       end
 
+      def self.get_missing_lines_from(lines, patterns=@@gitignore_patterns)
+        patterns.reject do |pattern|
+          lines.detect do |line|
+            line.strip.eql? pattern.strip
+          end
+        end
+      end
+
     end
   end
 end
